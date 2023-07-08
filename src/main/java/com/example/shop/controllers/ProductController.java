29b075773189de357b620688e424d88e5ab56556
n.polyakova <natli.d26.l8@gmail.com>
2023-07-09 01:14:44 +0300

    Add web

--- src/main/java/com/example/shop/controllers/ProductController.java
+++ src/main/java/com/example/shop/controllers/ProductController.java
@@ -7,6 +7,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.json.JSONObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Properties;
 
 @RestController
@@ -26,12 +29,13 @@ public class ProductController {
     static {
         try {
             props.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
-            props.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/shop");
-            props.setProperty("hibernate.connection.username", "nataliya");
-            props.setProperty("hibernate.connection.password", "qwert");
+            props.setProperty("hibernate.connection.url", "jdbc:postgresql://stampy.db.elephantsql.com:5432/nykbjfyu");
+            props.setProperty("hibernate.connection.username", "nykbjfyu");
+            props.setProperty("hibernate.connection.password", "BJ0QQMsy2UK45xWFY1PP9GNxa3yNr2jh");
             props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
             props.setProperty("hibernate.show_sql", "true");
             props.setProperty("hibernate.hbm2ddl.auto", "update");
+            props.setProperty("hibernate.connection.autocommit", "true");
 
             final Configuration config = new Configuration()
                     //.addResource("cfg.xml")
@@ -58,5 +62,20 @@ public class ProductController {
         transaction.commit();
 
         return "200";
+    };
+
+    @GetMapping("/all")
+    public ResponseEntity.BodyBuilder getProduct() {
+        Transaction transaction = session.beginTransaction();
+
+        var res = session.createNativeQuery("select * from product", ProductPojo.class).list();
+        transaction.commit();
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Allow-Origin",
+                "*");
+        return ResponseEntity
+                .ok()
+                .headers(responseHeaders);
     };
 }
