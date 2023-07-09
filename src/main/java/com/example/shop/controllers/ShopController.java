4664aac831102a693e18405a8e13da28e3056178
daniilsalokin <work.salokin@gmail.com>
2023-07-09 12:38:46 +0300

    fix shop responses

--- src/main/java/com/example/shop/controllers/ShopController.java
+++ src/main/java/com/example/shop/controllers/ShopController.java
@@ -2,6 +2,7 @@ package com.example.shop.controllers;
 
 import com.example.shop.models.ShopDto;
 import com.example.shop.models.ShopPojo;
+import jakarta.persistence.Entity;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -9,60 +10,52 @@ import org.hibernate.cfg.Configuration;
 import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Properties;
 
-import static com.example.shop.Configuration.createNewSession;
 
-@CrossOrigin(
-        origins = "http://localhost:63325",
-        methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.DELETE},
-        allowedHeaders = "Content-type"
-)
 @RestController
-@RequestMapping(path = "/shops")
+@Entity
+@RequestMapping(path = "/shop")
 public class ShopController {
 
-    private static SessionFactory factory;
-    private static final Properties props = new Properties();
+    private static Session session;
+    private static Properties props = new Properties();
 
-    public void setProps() {
-        props.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
-        props.setProperty("hibernate.connection.url", "jdbc:postgresql://stampy.db.elephantsql.com:5432/nykbjfyu");
-        props.setProperty("hibernate.connection.username", "nykbjfyu");
-        props.setProperty("hibernate.connection.password", "BJ0QQMsy2UK45xWFY1PP9GNxa3yNr2jh");
-        props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        props.setProperty("hibernate.show_sql", "true");
-        props.setProperty("hibernate.hbm2ddl.auto", "update");
-        props.setProperty("hibernate.connection.autocommit", "true");
-    }
+    static {
+        try {
+            props.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+            props.setProperty("hibernate.connection.url", "jdbc:postgresql://stampy.db.elephantsql.com:5432/nykbjfyu");
+            props.setProperty("hibernate.connection.username", "nykbjfyu");
+            props.setProperty("hibernate.connection.password", "BJ0QQMsy2UK45xWFY1PP9GNxa3yNr2jh");
+            props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            props.setProperty("hibernate.show_sql", "true");
+            props.setProperty("hibernate.hbm2ddl.auto", "update");
+            props.setProperty("hibernate.connection.autocommit", "true");
 
-    public void createFactory() {
-        setProps();
-        final Configuration config = new Configuration()
-                .addAnnotatedClass(com.example.shop.models.ShopPojo.class)
-                .addProperties(props);
+            final Configuration config = new Configuration()
+                    //.addResource("cfg.xml")
+                    .addAnnotatedClass(com.example.shop.models.ShopPojo.class)
+                    .addProperties(props);
+            SessionFactory factory = config.buildSessionFactory();
 
-        factory = config.buildSessionFactory();
+            session = factory.openSession();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @PostMapping("/add")
-    public @ResponseBody ResponseEntity<String> addShop(@RequestBody ShopDto dto) {
-        if (factory == null) {
-            createFactory();
-        }
-        Session session = factory.openSession();
+    public String createShop(@RequestBody ShopDto dto) {
         JSONObject object = new JSONObject(dto);
         Transaction transaction = session.beginTransaction();
         final ShopPojo pojo = new ShopPojo();
@@ -71,77 +64,49 @@ public class ShopController {
 
         session.persist(pojo);
         transaction.commit();
-        session.close();
 
-        HttpHeaders responseHeaders = new HttpHeaders();
-
-        return ResponseEntity.status(200).headers(responseHeaders).build();
-    }
-
-    @GetMapping("/all")
-    public @ResponseBody ResponseEntity<List<ShopPojo>> getShops() {
-        if (factory == null) {
-            createFactory();
-        }
-        Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-
-        var res = session.createNativeQuery("select * from shops", ShopPojo.class).list();
-        transaction.commit();
-        session.close();
-
-        HttpHeaders responseHeaders = new HttpHeaders();
-
-        return ResponseEntity.ok()
-                .headers(responseHeaders)
-                .body(res);
+        return "200";
     }
 
     @GetMapping("/{shopId}")
-    public @ResponseBody ResponseEntity<ShopPojo> getShop(@PathVariable long shopId) {
-        if (factory == null) {
-            createFactory();
-        }
-        Session session = createNewSession(factory);
+    public ShopPojo getShop(@PathVariable long shopId) {
         Transaction transaction = session.beginTransaction();
 
         var res = session.createNativeQuery("select * from shops where shop_id = " + shopId,
                 ShopPojo.class).uniqueResult();
         transaction.commit();
-        session.close();
 
-        HttpHeaders responseHeaders = new HttpHeaders();
-
-        return ResponseEntity.ok()
-                .headers(responseHeaders)
-                .body(res);
+        return res;
     }
 
 
-    @DeleteMapping("/delete/{shopId}")
-    public @ResponseBody ResponseEntity<String> deleteShop(@PathVariable Long shopId) {
-
-        ShopPojo rm_shop = new ShopPojo();
-        rm_shop.setShopId(shopId);
-
-        if (factory == null) {
-            createFactory();
-        }
-        Session session = factory.openSession();
+    @GetMapping("/all")
+    public List<ShopPojo> getAllShops() {
         Transaction transaction = session.beginTransaction();
 
+        var res = session.createNativeQuery("select * from shops", ShopPojo.class).list();
+        transaction.commit();
 
-        session.createNativeQuery("delete from shops where shop_id = :id", ShopPojo.class)
-                .setParameter("id", rm_shop.getShopId())
+        return res;
+    }
+
+    @DeleteMapping("/delete/{shopId}")
+    public String deleteProduct(@PathVariable long shopId) {
+        ShopPojo p = new ShopPojo();
+        p.setShopId(shopId);
+
+
+
+        Transaction transaction = session.beginTransaction();
+
+//        session.remove(p);//.createNativeQuery("delete from shops where shop_id = " + shopId, ShopPojo.class);
+//        transaction.commit();
+        session.createNativeQuery("delete from shops where shop_id = :id")
+                .setParameter("id", p.getShopId())
                 .executeUpdate();
         transaction.commit();
-        session.close();
 
-        HttpHeaders responseHeaders = new HttpHeaders();
-
-        return ResponseEntity
-                .status(204)
-                .headers(responseHeaders)
-                .build();
+        return "204";
     }
+
 }
