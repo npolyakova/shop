0c5bf7ef0232cf0063e33a8bc7dd04f872d0e846
n.polyakova <natli.d26.l8@gmail.com>
2023-07-11 13:40:33 +0300

    Fixes

--- src/main/java/com/example/shop/controllers/ProductController.java
+++ src/main/java/com/example/shop/controllers/ProductController.java
@@ -9,48 +9,58 @@ import org.hibernate.cfg.Configuration;
 import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Properties;
 
+@CrossOrigin(
+        origins = "http://localhost:63325",
+        methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.DELETE },
+        allowedHeaders = "Content-type"
+)
 @RestController
 @RequestMapping(path = "/product")
 public class ProductController {
 
-    private static Session session;
+    private static SessionFactory factory;
     private static final Properties props = new Properties();
 
-    static {
-        try {
-            props.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
-            props.setProperty("hibernate.connection.url", "jdbc:postgresql://stampy.db.elephantsql.com:5432/nykbjfyu");
-            props.setProperty("hibernate.connection.username", "nykbjfyu");
-            props.setProperty("hibernate.connection.password", "BJ0QQMsy2UK45xWFY1PP9GNxa3yNr2jh");
-            props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-            props.setProperty("hibernate.show_sql", "true");
-            props.setProperty("hibernate.hbm2ddl.auto", "update");
-            props.setProperty("hibernate.connection.autocommit", "true");
+    public void setProps() {
+        props.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+        props.setProperty("hibernate.connection.url", "jdbc:postgresql://stampy.db.elephantsql.com:5432/nykbjfyu");
+        props.setProperty("hibernate.connection.username", "nykbjfyu");
+        props.setProperty("hibernate.connection.password", "BJ0QQMsy2UK45xWFY1PP9GNxa3yNr2jh");
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        props.setProperty("hibernate.show_sql", "true");
+        props.setProperty("hibernate.hbm2ddl.auto", "update");
+        props.setProperty("hibernate.connection.autocommit", "true");
+    }
 
-            final Configuration config = new Configuration()
-                    .addAnnotatedClass(com.example.shop.models.ProductPojo.class)
-                    .addProperties(props);
-            SessionFactory factory = config.buildSessionFactory();
+    public void createFactory() {
+        setProps();
+        final Configuration config = new Configuration()
+                .addAnnotatedClass(com.example.shop.models.ProductPojo.class)
+                .addProperties(props);
 
-            session = factory.openSession();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        factory = config.buildSessionFactory();
     }
 
     @PostMapping("/add")
-    public void createProduct(@RequestBody ProductDto dto) {
+    public @ResponseBody ResponseEntity<String> createProduct(@RequestBody ProductDto dto) {
+        if (factory == null) {
+            createFactory();
+        }
+        Session session = factory.openSession();
         JSONObject object = new JSONObject(dto);
         Transaction transaction = session.beginTransaction();
         final ProductPojo pojo = new ProductPojo();
@@ -59,34 +69,44 @@ public class ProductController {
 
         session.persist(pojo);
         transaction.commit();
+        session.close();
 
         HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("Access-Control-Allow-Origin", "*");
-        responseHeaders.set("Access-Control-Allow-Headers", "*");
 
-        ResponseEntity.ok().headers(responseHeaders);
+        return ResponseEntity.status(204).headers(responseHeaders).build();
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<ProductPojo>> getProduct() {
+    public @ResponseBody ResponseEntity<List<ProductPojo>> getProduct() {
+        if (factory == null) {
+            createFactory();
+        }
+        Session session = factory.openSession();
         Transaction transaction = session.beginTransaction();
 
         var res = session.createNativeQuery("select * from product", ProductPojo.class).list();
         transaction.commit();
+        session.close();
 
         HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("Access-Control-Allow-Origin", "*");
+        //responseHeaders.set("Access-Control-Allow-Origin", "*");
 
         return ResponseEntity.ok()
                 .headers(responseHeaders)
                 .body(res);
     }
 
-    @DeleteMapping("/delete")
-    public void deleteProduct() {
+    @DeleteMapping("/delete/{productId}")
+    public void deleteProduct(@PathVariable Integer productId) {
+        if (factory == null) {
+            createFactory();
+        }
+        Session session = factory.openSession();
         Transaction transaction = session.beginTransaction();
 
-        session.createNativeQuery("delete from product where id<1000", ProductPojo.class).list();
+        session.createNativeQuery("delete from product where id=" + productId, ProductPojo.class).executeUpdate();
+
         transaction.commit();
+        session.close();
     }
 }
