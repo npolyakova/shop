2d0b2d53b9d529c42ae56db1c898709b02d8e599
n.polyakova <natli.d26.l8@gmail.com>
2023-07-19 11:55:39 +0300

    Merge servers

--- src/main/java/com/example/shop/controllers/ShopController.java
+++ src/main/java/com/example/shop/controllers/ShopController.java
@@ -9,35 +9,27 @@ import org.hibernate.cfg.Configuration;
 import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Properties;
 
 import static com.example.shop.Configuration.buildFactory;
 import static com.example.shop.Configuration.createNewSession;
 import static com.example.shop.ShopHandler.checkFirstLetter;
 import static com.example.shop.ShopHandler.checkLength;
 
-@CrossOrigin(
-        origins = "http://localhost:63342",
-        methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.DELETE},
-        allowedHeaders = "Content-type"
-)
 @RestController
 @RequestMapping(path = "/shops")
 public class ShopController extends Configuration {
 
-    private static SessionFactory factory = buildFactory();
+    private static final SessionFactory factory = buildFactory();
 
     @PostMapping("/add")
     public @ResponseBody ResponseEntity<String> addShop(@RequestBody ShopDto dto) {
