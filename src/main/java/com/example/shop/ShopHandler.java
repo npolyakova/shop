0a0c7d9d0b487275260f51a79e0a840b09fa2319
DESKTOP-PMQOTRO\Николай <dmitriyrostov2016@gmail.com>
2023-07-26 22:09:35 +0300

    Unit test commit

--- src/main/java/com/example/shop/ShopHandler.java
+++ src/main/java/com/example/shop/ShopHandler.java
@@ -1,5 +1,7 @@
 package com.example.shop;
 
+import com.example.shop.models.ShopDto;
+
 public class ShopHandler {
 
     public static boolean checkLength(String name, int count) {
@@ -9,5 +11,10 @@ public class ShopHandler {
     public static boolean checkFirstLetter(String name) {
         String firstLetter = name.substring(0, 1);
         return !firstLetter.equals(firstLetter.toLowerCase());
+    }
+
+
+    public Object addShop(ShopDto dto) {
+        return dto;
     }
 }
